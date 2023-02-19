f0ebdae5b54557cc2dd6a9a60cadd340b72001bc
Vishal Shende <vishal.shende@gmail.com>
2023-02-19 16:50:10 -0500

    Added test cases

---
+++ src/main/java/com/vshende/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.vshende.calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class Calculator {
+
+     public int sum(int a, int b) {
+
+          return a + b;
+
+     }
+
+}
